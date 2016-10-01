1bcfbd5ef3ffd207971bea662c9dde3ccb96105a
mparab01 <infoeastindia1@gmail.com>
2016-09-30 22:01:34 -0500

    Python_Dictionary
    https://docs.python.org/3/tutorial/datastructures.html#dictionaries

---
+++ Dict.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[2]:
+
+d = {}
+
+
+# In[3]:
+
+d['Jack']='Allan'
+
+
+# In[4]:
+
+d
+
+
+# In[5]:
+
+d['John']='Joddy'
+
+
+# In[6]:
+
+d
+
+
+# In[7]:
+
+for k,v in d.items():
+    print (k,v)
+
+
+# In[8]:
+
+d['dog'] = 42
+
+
+# In[9]:
+
+d
+
+
+# In[10]:
+
+d ['item'] = range(10)
+
+
+# In[11]:
+
+d
+
+
+# In[12]:
+
+d['item'][2]
+
+
+# In[14]:
+
+d.keys()
+
+
+# In[15]:
+
+d.values()
+
+
+# In[16]:
+
+d.items()
+
+
+# In[ ]:
+
+
+
